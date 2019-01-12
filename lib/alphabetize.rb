b7e26a023c55742c6bbb9e9de50676a10f3f9685
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-12 20:05:16 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,35 +7,3 @@ def alphabetize(arr)
     end
   end
 end
-
-
-=begin
-
-def alphabetize(sentences_array)
-  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  sentences_array.sort_by do |sentence|
-    sentence.split("").map do |character|
-      esperanto.index(character)
-    end
-  end
-end
-
-=begin
-esp_alph = " abcĉĉdefgĝĝhĥĥijĵĵklmnoprsŝŝtuŭŭvz"
-
-arr = ["abc\u0302a", "abĉa","abca" ]
-p arr.sort_by {|string| string.chars.map{|c| esp_alph.index(c)}}]
-
-
-
-ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
-ascii = "@-\\"
-arr.sort_by{|string| string.tr(esp_alph, ascii)}
-=end
-
-#students.sort { |a, b| a <=> b }
-#str = '1654AaBcDddeeFF'
-#order_array = [*'0'..'9',*'a'..'z',*'A'..'Z']
-#str.each_char.sort_by { |c| order_array.index(c) }.join
-
-#puts alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
