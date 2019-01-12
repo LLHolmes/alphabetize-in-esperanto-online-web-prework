c587e55a38c43e99e185f019aa83f30bb19c3f85
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-12 19:16:55 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_array = esperanto.split(//)
-  arr.sort_by! { |obj| esperanto.each_char.index(obj) }
+  arr.sort_by! { |obj| esp_array.index(obj) }
   arr
 end
 
@@ -9,3 +9,5 @@ end
 #str = '1654AaBcDddeeFF'
 #order_array = [*'0'..'9',*'a'..'z',*'A'..'Z']
 #str.each_char.sort_by { |c| order_array.index(c) }.join
+
+#arr.sort_by! { |obj| esperanto.each_char.index(obj) }
